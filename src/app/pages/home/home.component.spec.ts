ed391a73eac11ab4e48dbfe48c8a8dd85423a448
Marcelo Hosan <marcelo.hosan@tsoftglobal.com>
2023-05-27 10:45:10 -0300

    ejemplo completo de observable

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { HomeComponent } from "./home.component";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { Book } from 'src/app/models/book.model';
+import { of } from 'rxjs';
 
 
 describe('Home component', ()=>{
@@ -32,5 +34,17 @@ describe('Home component', ()=>{
     it('should create', ()=>{
         expect(component).toBeTruthy();
     });
+
+    it('getBooks get books from the subscription',()=>{
+        const bookService = fixture.debugElement.injector.get(BookService);
+        const listBook: Book[] = [];                //se pasa un array vacio, pero tambien se podria pasar una const local con
+                                                    //el array con datos.
+        const spy1 = spyOn(bookService, 'getBooks').and.returnValue(of(listBook));//Se utiliza el operador "of" de la libreria
+                                                    //RxJs para transformar en observable el argumento listBook
+        component.getBooks();
+        expect(spy1).toHaveBeenCalled();
+        expect(component.listBook.length).toBe(0);
+    });
+
 });
 
